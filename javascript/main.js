bc34d92065717b3f513a3c9c2461e21b52a6502a
Matteo Fruianu <matteofruianu@gmail.com>
2020-11-25 15:17:13 +0100

    Star integer rating

--- javascript/main.js
+++ javascript/main.js
@@ -8,6 +8,7 @@ const app = new Vue ({
 
       movies: [], //inizializzo una variabile come array vuoto per mettere i movies ottenuti dal db
       search: '', //v-model sulla barra di ricerca per filtrare i risultati
+     
       
     },
 
@@ -18,18 +19,24 @@ const app = new Vue ({
         axios.get('https://api.themoviedb.org/3/search/movie?', {
           params: {
             api_key: '8c77d5df1eea7f2bd6be310c611fe245',
-            query: this.search
+            query: this.search,
+            language: 'it-IT'
           }
         })
         .then(response => {
-          console.log(response.data.results);
-          this.movies = response.data.results
+          this.movies = response.data.results;
+          this.movieVote = response.data.results.vote_average;
         })
         .catch(error => {
           console.log(error);
         })
         
-      } 
+      },
+      
+      
+      getVote(vote) {
+        return Math.round((vote / 2));
+      }
 
     } 
 
